672605e163d94c272e7c349e68ffd6c08f022bd4
SanketBhandarii <bhandarisanketp@gmail.com>
2024-02-22 22:28:52 +0530

    Animation added to menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Navbar.css';
 function Navbar() {
   return (
    <nav className='flex  justify-around h-16 items-center bg-slate-100'>
@@ -7,12 +7,12 @@ function Navbar() {
        <h1 className='text-logo-color font-bold cursor-pointer'>Your logo</h1>
      </div>
      <div className='menu-list'>
-        <ul className='flex items-center gap-5 font-medium'>
-          <a className='cursor-pointer'><li>Home</li></a>
-          <a className='cursor-pointer'><li>About us</li></a>
-          <a className='cursor-pointer'><li>Portfolio</li></a>
-          <a className='cursor-pointer'><li>Info</li></a>
-          <a className='cursor-pointer'><li>Contact us</li></a>
+        <ul className='flex items-center gap-7 font-medium'>
+          <a className='cursor-pointer text-logo-color py-5'><li>Home</li></a>
+          <a className='cursor-pointer hover:text-logo-color transition duration-75 py-5'><li>About us</li></a>
+          <a className='cursor-pointer hover:text-logo-color transition duration-75 py-5'><li>Portfolio</li></a>
+          <a className='cursor-pointer hover:text-logo-color transition duration-75 py-5'><li>Info</li></a>
+          <a className='cursor-pointer hover:text-logo-color transition duration-75 py-5'><li>Contact us</li></a>
         </ul>
      </div>
    </nav>
