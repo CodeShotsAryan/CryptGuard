6dd087b4465132e262c8d262f38b4ac988445e53
SanketBhandarii <bhandarisanketp@gmail.com>
2024-02-23 00:05:54 +0530

    Navbar Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import HeroImg from "../assets/HeroImg.jpg";
 import './Hero.css';
 function Hero() {
   return (
-    <section className="flex h-96 items-center justify-center p-36 gap-7">
+    <section className="flex h-96 items-center justify-center gap-7">
       <div className="flex flex-col gap-5">
         <h1 className="font-bold text-4xl w-60">Welcome to <span className="text-logo-color">CRYPTGUARD</span></h1>
         <p className="w-text1-width">
