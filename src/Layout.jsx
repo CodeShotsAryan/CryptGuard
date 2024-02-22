502e2257a0313ef5eb11a6cec19fb6e05ed0be7d
SanketBhandarii <bhandarisanketp@gmail.com>
2024-02-22 23:05:30 +0530

    Navbar Updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero';
 function Layout() {
   return (
     <div>
       <Navbar />
+      <Hero/>
       {/* <Outlet /> */}
     </div>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+// const image = require("../assets/HeroImg.jpg")
+function Hero(){
+  return(
+    <section>
+      <div>
+      <h1 className="font-bold text-4xl w-60">Welcome to CRYPTGUARD</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis obcaecati, fuga quisquam ipsa illum id placeat laborum quaerat aut, asperiores hic culpa, repellendus dolorem alias quidem recusandae fugiat numquam quas.</p>
+      </div>
+      <div>
+        
+      </div>
+    </section>
+  );
+}
+export default Hero;
