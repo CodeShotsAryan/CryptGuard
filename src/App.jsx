e12365f57b062fe9762c9da35e08332106d3af92
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-02-22 20:46:53 +0530

    final routes by avi

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='font-bold'>hello this is code </h1>  
-      <h1 className='font-bold'>hello this is code </h1>  
-      <h1 className='font-bold'>hello this is updated by avishkar</h1>  
+  
     </>
   )
   }
