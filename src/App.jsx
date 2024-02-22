2347cede936a84fe394289208e07b9d61f5a35d9
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-02-22 12:22:09 +0530

    aryan2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1 className='font-bold'>hello this is code </h1>  
-      
+      <h1 className='font-bold'>hello this is code </h1>  
     </>
   )
   }
