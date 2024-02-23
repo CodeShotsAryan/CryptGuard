8eee460ecd6befdb62d6674344f9a58ff05e8317
SanketBhandarii <bhandarisanketp@gmail.com>
2024-02-23 10:30:46 +0530

    Updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
+
 function Layout() {
   return (
     <div>
