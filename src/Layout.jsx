74f259cd5aa7bce6034833ad459b40b9f9da206b
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-02-24 00:12:36 +0530

    step back

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar';
-import { useSelector } from 'react-redux';
-import Dashboard from './components/Dashboard/Dashboard';
 function Layout() {
-  // const [login , setLogin] = useState(false)
-  const auth = useSelector(state=>state.User.status);
-  return auth? (
-    <Dashboard/>
-  ) : (
-    <>
-    <Navbar/>
-    <Dashboard/>
-    </>
-  )
+  const [login , setLogin] = useState(false)
+  return (
+     
+    <div  >
+    {
+      login ? null : <Navbar />
+    }
+      <Outlet/> 
+    </div>
+  );
 }
 
 export default Layout;
