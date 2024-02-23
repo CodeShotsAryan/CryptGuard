e64d8ddebb34ea022aa342284ed62da203ccdb34
SanketBhandarii <bhandarisanketp@gmail.com>
2024-02-23 20:03:12 +0530

    Updated by Sanket

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,18 +1,14 @@
-import React, { useState , useContext } from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import React, { useState, useContext } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 function Layout() {
-  const [login , setLogin] = useState(false)
   // const {isLogged , setIsLogged} = useContext(UserContext)
   return (
-     
-    <div  >
-    {
-      isLogged ? null : <Navbar />
-    }
-      <Outlet/> 
+    <div>
+      <Navbar />
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
