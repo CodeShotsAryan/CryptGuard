80cda9ec9be0128b2a58fda71287f01766a2011f
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-02-23 20:46:45 +0530

    final change by avi

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 function Layout() {
   const [login , setLogin] = useState(false)
   return (
@@ -11,17 +10,6 @@ function Layout() {
       login ? null : <Navbar />
     }
       <Outlet/> 
-=======
-import React, { useState, useContext } from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-function Layout() {
-  // const {isLogged , setIsLogged} = useContext(UserContext)
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
->>>>>>> e64d8ddebb34ea022aa342284ed62da203ccdb34
     </div>
   );
 }
