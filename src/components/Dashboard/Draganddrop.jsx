760fae48e169fb78ce4689aae08f14760dc7e19e
SanketBhandarii <bhandarisanketp@gmail.com>
2024-02-25 22:30:17 +0530

    Dragandrop design updated by sanket

--- src/components/Dashboard/Draganddrop.jsx
+++ src/components/Dashboard/Draganddrop.jsx
@@ -1,10 +1,10 @@
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router";
+import './Draganddrop.css';
 const Draganddrop = () => {
   const [files, setFiles] = useState(null);
   const inputRef = useRef();
   const navigate = useNavigate();
-
 
   const handleDragOver = (event) => {
     event.preventDefault();
@@ -12,44 +12,46 @@ const Draganddrop = () => {
 
   const handleDrop = (event) => {
     event.preventDefault();
-    setFiles(event.dataTransfer.files)
+    setFiles(event.dataTransfer.files);
   };
-  
+
   const handleUpload = () => {
     const formData = new FormData();
     formData.append("Files", files);
-    
-    console.log(formData);
-    console.log(files)
-    for (const value of formData.values()) {
-        console.log(value);
-    }
-    navigate('/dashboard');
 
-};
-  if (files) return (
-    <div className="uploads">
+    console.log(formData);
+    console.log(files);
+    for (const value of formData.values()) {
+      console.log(value);
+    }
+    navigate("/dashboard");
+  };
+  if (files)
+    return (
+      <div className="uploads">
         <ul>
-            {Array.from(files).map((file, idx) => <li key={idx}>{file.name}</li> )}
+          {Array.from(files).map((file, idx) => (
+            <li key={idx}>{file.name}</li>
+          ))}
         </ul>
         <div className="actions">
-            <button onClick={() => setFiles(null)}>Cancel</button>
-            <button onClick={handleUpload}>Upload</button>
+          <button onClick={() => setFiles(null)}>Cancel</button>
+          <button onClick={handleUpload}>Upload</button>
         </div>
-    </div>
-  )
+      </div>
+    );
 
   return (
     <>
-    <div className="dropzone-container">
-        <div 
-            className="dropzone"
-            onDragOver={handleDragOver}
-            onDrop={handleDrop}
+      <div className="dropzone-container bg-slate-800">
+        <div
+          className="dropzone"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
         >
-          <h1>Drag and Drop Files to Upload</h1>
-          <h1>Or</h1>
-          <input 
+          <h1 className="text-slate-200">Drag and Drop Files to Upload</h1>
+          <h1 className="text-slate-200">Or</h1>
+          <input
             type="file"
             multiple
             onChange={(event) => setFiles(event.target.files)}
@@ -57,9 +59,9 @@ const Draganddrop = () => {
             accept="image/png, image/jpeg"
             ref={inputRef}
           />
-          <button onClick={() => inputRef.current.click()}>Select Files</button>
+          <button onClick={() => inputRef.current.click()} className="w-48 h-14">Select Files</button>
         </div>
-    </div>
+      </div>
     </>
   );
 };
