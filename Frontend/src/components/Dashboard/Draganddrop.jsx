05ec5d1b647d1df78fce8145703aa7b9fd872538
ARYAN PATIL <125760276+ARYANPATILSOLOCODER11@users.noreply.github.com>
2024-03-01 16:21:06 +0530

    frontend

---
+++ Frontend/src/components/Dashboard/Draganddrop.jsx
@@ -0,0 +1,85 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router";
+import './Draganddrop.css';
+import { encryptData  } from "../../../../backend/utils/cryptoUtils";
+import { generateKey } from "../../../../backend/utils/keyManagment";
+import {uploadFile} from "../../../../backend/utils/uploadFile"
+
+
+// function handleFiles(){
+//   Array.from(files).forEach(file => {
+//     const reader = new FileReader();
+//     reader.readAsBinaryString(file);
+//     render.onload = function(){
+//       const {encryptedData , key ,file.name} = Enc
+//     }
+//   });
+// }
+const Draganddrop = () => {
+  const [files, setFiles] = useState(null);
+  const inputRef = useRef();
+  const navigate = useNavigate();
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setFiles(event.dataTransfer.files);
+  };
+
+  const handleUpload = () => {
+    const formData = new FormData();
+    formData.append("Files", files);
+
+    console.log(formData);
+    console.log(files);
+    for (const value of formData.values()) {
+      console.log(value);
+    }
+    navigate("/dashboard");
+  };
+
+  
+  if (files)
+    return (
+      <div className="uploads">
+        <ul>
+          {Array.from(files).map((file, idx) => (
+            <li key={idx}>{file.name}</li>
+          ))}
+        </ul>
+        <div className="actions">
+          <button onClick={() => setFiles(null)}>Cancel</button>
+          <button onClick={handleUpload}>Upload</button>
+        </div>
+      </div>
+    );
+
+  return (
+    <>
+      <div className="dropzone-container bg-slate-800">
+        <div
+          className="dropzone"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
+          <h1 className="text-slate-200">Drag and Drop Files to Upload</h1>
+          <h1 className="text-slate-200">Or</h1>
+          <input
+            type="file"
+            multiple
+            onChange={(event) => setFiles(event.target.files)}
+            hidden
+            accept="image/png, image/jpeg"
+            ref={inputRef}
+          />
+          <button onClick={() => inputRef.current.click()} className="w-48 h-14">Select Files</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Draganddrop;
