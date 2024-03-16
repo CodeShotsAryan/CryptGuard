acddb8622f941d30aa599c8844ab2e3fc092c3bc
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-03-16 19:02:25 +0530

    sidenav  updates

--- Frontend/src/components/Dashboard/SideNav.jsx
+++ Frontend/src/components/Dashboard/SideNav.jsx
@@ -13,15 +13,17 @@ function SideNav() {
     <div className="sidenav p-7">
       <div className="sidenav-1">
         <div className="sidenav-logo">Logo</div>
-        <i
-          className="fa-solid fa-circle-user cursor-pointer text-3xl transition-all duration-200"
-          style={{ color: "#8532fa" }}
-        ></i>
-        <i className="fa-solid fa-house cursor-pointer transition-all duration-200 hover:text-logo-color"></i>
+        <Link to="/dashboard">
+          <i className="fa-solid fa-house cursor-pointer transition-all duration-200 hover:text-logo-color"></i>
+        </Link>
         <Link to="/fileslist">
           <i className="fa-solid fa-folder-closed cursor-pointer transition-all duration-200 hover:text-logo-color"></i>
         </Link>
         <i className="fa-solid fa-lightbulb cursor-pointer transition-all duration-200 hover:text-logo-color"></i>
+        <i
+          className="fa-solid fa-circle-user cursor-pointer text transition-all duration-200 hover:text-logo-color"
+          // style={{ color: "#8532fa" }}
+        ></i>
       </div>
       <Link to="/" className="sidenav-2" onClick={handleMakeFalse}>
         <i className="fa-solid fa-right-from-bracket cursor-pointer text-red-600 transition-all duration-200 hover:text-icon-color"></i>
