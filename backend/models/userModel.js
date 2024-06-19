4f67f31fbd6a4409f3d15a576e8998e39c05ebea
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-06-20 00:26:03 +0530

    user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,5 +19,9 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-    bio:String,
+    profilePictureURL:String,
 })
+
+const User = mongoose.model("User",userSchema)
+
+module.exports = User
